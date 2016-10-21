e97780f7384656cfd0d4dc22d9b2784de1e556c3
TheSputnik <robert.reinalter@edu.fh-joanneum.at>
2016-10-21 13:48:29 +0200

    first commit

---
+++ test/at/fhj/itm/swq/debug/IntegerLinkedListTest.java
@@ -0,0 +1,50 @@
+package at.fhj.itm.swq.debug;
+
+import java.util.logging.Logger;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+
+import at.fhj.itm.swq.debug.IntegerLinkedList;
+
+
+public class IntegerLinkedListTest
+{
+    @Rule
+    public TestName testname = new TestName();
+    final static Logger logger = Logger.getLogger("Testlogger");
+    
+    private IntegerLinkedList list;
+    
+    @Before
+    public void setup()
+    {
+        logger.info("Starting test: " + testname.getMethodName());
+        list = new IntegerLinkedList();
+        list.add(7).add(3).add(11);        
+    }
+    
+    @Test
+    public void testGet()
+    {
+        Assert.assertEquals(7,list.get(0));
+        Assert.assertEquals(3,list.get(1));
+        Assert.assertEquals(11,list.get(2));
+    }
+
+   @Test(expected=IllegalArgumentException.class)
+   public void testGet_WithTooLargeIndex()
+   {
+       list.get(3);
+   }
+   
+   
+   @Test
+   public void testToString()
+   {
+       Assert.assertEquals("{7,3,11}", list.toString());
+   }
+}
